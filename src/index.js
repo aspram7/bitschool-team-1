d7435d479afe801ff7818333efdffe16edb64010
Aspram <aspram.markosyan7@gmail.com>
2021-01-15 01:45:25 +0400

    add sass

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import "./assets/fonts/font-icons.css";
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
