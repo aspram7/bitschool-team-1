39f51a6d9628818b24062ab039a4185b315c9e26
Aspram <aspram.markosyan7@gmail.com>
2021-01-15 02:42:21 +0400

    remove redundant codes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// import "./assets/fonts/icons/font-icons.css";
-// import "./assets/fonts/quicksand/style.css";
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
