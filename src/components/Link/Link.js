f429271bb65c2e5f9459b5627bce7fb261664252
ArturAghakaryan <35149926+ArturAghakaryan@users.noreply.github.com>
2021-01-16 16:37:08 +0400

    Revert "Header"

--- src/components/Link/Link.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./Link.scss";
-
-const Link = ({ children, className , href }) => {
-  return <a href={href} className={`${className}`}>{children}</a>;
-};
-
-export default Link;
