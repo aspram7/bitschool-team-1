28896468bc9721815726bfbbe971244c6ac08698
Aspram <aspram.markosyan7@gmail.com>
2021-01-16 14:55:13 +0400

    beautify the code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
-import Button from "./components/Button/Button";
 
 import "./App.css";
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./Layout.scss";
 
 const Layout = ({ children }) => {
-  return <main className="layout">{children}</main>;
+  return <main className="main-component">{children}</main>;
 };
 
 export default Layout;
