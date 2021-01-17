43075e9de973e7c78da866a3288ec63399b15d25
Aspram <aspram.markosyan7@gmail.com>
2021-01-17 22:07:44 +0400

    Created Get A Pet section

--- src/containers/Sections/Sections.js
+++ src/containers/Sections/Sections.js
@@ -2,15 +2,17 @@ import React from "react";
 
 import Welcome from "./Welcome/Welcome";
 import WhatWeDo from "./WhatWeDo/WhatWeDo";
+import GetAPet from "./GetAPet/GetAPet";
 
 const Sections = () => {
-    return(
-        <>
-        <Welcome />
-        <WhatWeDo />
-        </>
-    );
-}
+  return (
+    <>
+      <Welcome />
+      <WhatWeDo />
+      <GetAPet />
+      <div style={{ marginBottom: "800px" }}></div>
+    </>
+  );
+};
 
-
-export default  Sections;
+export default Sections;
