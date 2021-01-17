567765eb1d37b0ec7244e4d4245a42dcc1abb1ba
artur <artur@weem.tech>
2021-01-17 20:02:59 +0400

    crate join us section

---
+++ src/components/Box/SliderBox/SliderBox.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Link from "../../Link/Link";
+
+import "./SliderBox.scss";
+
+const SliderBox = ({ img, title, description}) => {
+  return (
+    <div className="slider-box">
+      <div className="slider-box-content">
+        <div className="slider-box-img">
+            <img src={img.url} alt={img.alt} />
+        </div>
+        <div className="slider-box-info">
+          <p className="slider-box-desc">{description}</p>
+          <h3 className="slider-box-title">{title}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SliderBox;
