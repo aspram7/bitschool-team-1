f44862bb5ecb1f10021463a6578698c11bb39aeb
Aspram <aspram.markosyan7@gmail.com>
2021-01-17 19:08:34 +0400

    Created Register

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Login from "./Login/Login";
+import Register from "./Register/Register";
 
 import "./Auth.scss";
 
 const Auth = () => {
-  return <Login />;
+  const [activePage, setActivePage] = useState("register");
+
+  const onRedirect = (page) => {
+    setActivePage(page);
+  };
+
+  return <div className="auth">{activePage === "register" ? <Register onRedirect={onRedirect} /> : <Login onRedirect={onRedirect} />}</div>;
 };
 
 export default Auth;
---
+++ src/containers/Auth/Register/Register.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+
+import Input from "../../../components/Input/Input";
+import Button from "../../../components/Button/Button";
+import Checkbox from "../../../components/Checkbox/Checkbox";
+
+import { validateEmail, validatePassword } from "../../../utils/validate";
+
+import "./Register.scss";
+
+const Register = (props) => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const onClickButton = () => {
+    const obj = { name: "", email: "", password: "", confirmPassword: "" };
+    if (inputs.name && validateEmail(inputs.email) && validatePassword(inputs.password) && inputs.password === inputs.confirmPassword) {
+      setErrors(obj);
+      alert(`Name: ${inputs.name}\nEmail: ${inputs.email}\nPassword: ${inputs.password}`);
+    } else {
+      switch (false) {
+        case !!inputs.name:
+          setErrors({ ...obj, name: "Name can't be empty" });
+          break;
+        case validateEmail(inputs.email):
+          setErrors({ ...obj, email: "Please enter a valid email address." });
+          break;
+        case validatePassword(inputs.password):
+          setErrors({ ...obj, password: "Your password must be at least 8 characters" });
+          break;
+        case inputs.password === inputs.confirmPassword:
+          setErrors({ ...obj, confirmPassword: "Passwords don't match" });
+          break;
+        default:
+          setErrors(obj);
+      }
+    }
+  };
+
+  const onRedirect = () => {
+    props.onRedirect("login");
+  };
+
+  return (
+    <div className="login-reg-component">
+      <h1>Register Form</h1>
+      <Input
+        labelName="Full Name"
+        label="name-form"
+        type="text"
+        name="name"
+        value={inputs.name}
+        placeholder="Enter Your Name..."
+        onChange={onChange}
+      />
+      <Input
+        labelName="Email"
+        label="email-form"
+        type="text"
+        name="email"
+        value={inputs.email}
+        placeholder="Enter Your Email..."
+        errorMessage={errors.email}
+        onChange={onChange}
+      />
+      <Input
+        labelName="Password"
+        label="password-form"
+        type="password"
+        name="password"
+        value={inputs.password}
+        placeholder="Enter Your Password..."
+        errorMessage={errors.password}
+        onChange={onChange}
+      />
+      <Input
+        labelName="Confirm Password"
+        label="confirm-password-form"
+        type="password"
+        name="confirmPassword"
+        value={inputs.confirmPassword}
+        placeholder="Confirm Your Password..."
+        errorMessage={errors.confirmPassword}
+        onChange={onChange}
+      />
+      <Button onClick={onClickButton} style={{ marginTop: "10px" }}>
+        REGISTER
+      </Button>
+      <div className="login-reg">
+        Already have an account?
+        <span className="blue-link" onClick={onRedirect}>
+          Login Now
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
