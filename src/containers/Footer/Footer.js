51ec6c6d928661449bee5e997ec59b77b9ddad16
Aspram <aspram.markosyan7@gmail.com>
2021-01-19 18:38:08 +0400

    Created product

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,20 +1,17 @@
 import React from "react";
 
-import Link from "../../components/Link/Link";
 import FooterTop from "./FooterTop/FooterTop";
 import FooterBottom from "./FooterBottom/FooterBottom";
-
 
 import "./Footer.scss";
 
 const Footer = () => {
-   return(
-      <div className="footer-main">
-         <FooterTop /> 
-         <FooterBottom /> 
-      </div>
-   )
+  return (
+    <div className="footer-main">
+      <FooterTop />
+      <FooterBottom />
+    </div>
+  );
 };
 
 export default Footer;
-
